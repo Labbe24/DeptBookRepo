cedd3fafeef046215aa7efa484f96b2ec21cbb22
Lasse Andresen <lasseandresen.1996@hotmail.com>
2020-10-24 17:29:47 +0200

    Added a Datagrid to MainWindow.

--- DeptBook/ViewModels/MainWindowViewModel.cs
+++ DeptBook/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using DeptBook.Models;
+using Prism.Mvvm;
 
 namespace DeptBook.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
+        private ObservableCollection<Debtor> debtors;
+
+        public MainWindowViewModel()
+        {
+            debtors = new ObservableCollection<Debtor>();
+        }
+
+        public ObservableCollection<Debtor> Debtors 
+        {
+            get
+            {
+                return debtors;
+            }
+            set
+            {
+                debtors = value;
+            }
+        }
     }
 }
